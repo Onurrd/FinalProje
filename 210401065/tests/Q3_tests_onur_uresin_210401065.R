16c7973abd3404e77eb83df6b702b723a6c83121
Onur <onur.uresin@hotmail.com>
2024-01-19 15:25:39 +0300

    Q3 , testleri ile birlikte tamamlandı.

---
+++ 210401065/tests/Q3_tests_onur_uresin_210401065.R
@@ -0,0 +1,7 @@
+library(testthat)
+
+# Test 1: substat_type sütununda farklı değerlerin bulunup bulunmadığını kontrol edin
+test_that("substat_type sütununda farklı değerlerin bulunup bulunmadığını kontrol edin", {
+  expect_true(length(unique(myDF$substat_type)) > 1)
+})
+
